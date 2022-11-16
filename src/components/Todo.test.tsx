cb4a8e15fca33ae45a45828579f84926d21207ec
Lucas Ramos <lucasnramos@gmail.com>
2022-11-16 12:50:03 -0300

    feat: Todo initial interface and tests update

--- src/components/Todo.test.tsx
+++ src/components/Todo.test.tsx
@@ -1,26 +1,40 @@
-import { cleanup, render, screen } from "@testing-library/react";
-import Todo from "./Todo";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Todo, { TodoProps } from "./Todo";
 
 afterAll(cleanup);
 
+const mockTodo: TodoProps = {
+  completed: false,
+  description: "todo description",
+};
+
 describe("Showing todo", () => {
   test("Should render without errors", () => {
-    const { baseElement } = render(<Todo />);
+    const { baseElement } = render(<Todo {...mockTodo} />);
     expect(baseElement).toBeInTheDocument();
   });
 
   test("should show a checkbox to toggle todo", () => {
-    render(<Todo />);
-    expect(screen.getByTestId("toggle-todo")).toBeInTheDocument();
+    render(<Todo {...mockTodo} />);
+    expect(getToggleTodo()).toBeInTheDocument();
   });
 
   test("should show the todo's description", () => {
-    render(<Todo />);
-    expect(screen.getByText(/todo description/i)).toBeInTheDocument();
+    render(<Todo {...mockTodo} />);
+    expect(getTodoDescription()).toBeInTheDocument();
   });
 
   test("should have button to delete todo", () => {
-    render(<Todo />);
+    render(<Todo {...mockTodo} />);
     expect(screen.getByTestId("delete-todo")).toBeInTheDocument();
   });
 });
+
+function getToggleTodo() {
+  return screen.getByTestId("toggle-todo");
+}
+
+function getTodoDescription(description = /todo description/i) {
+  return screen.getByText(description);
+}
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,11 +1,16 @@
-function Todo() {
+export interface TodoProps {
+  completed: boolean;
+  description: string;
+}
+
+const Todo: React.FC<TodoProps> = ({ description, completed }) => {
   return (
     <div>
-      <input type="checkbox" data-testid="toggle-todo" />
-      <p>todo description</p>
+      <input type="checkbox" data-testid="toggle-todo" checked={completed} />
+      <p>{description}</p>
       <button data-testid="delete-todo">Delete Todo</button>
     </div>
   );
-}
+};
 
 export default Todo;
